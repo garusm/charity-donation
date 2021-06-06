ddcc2e23bddd38b3b095abc17a9f7e9f3adb4eb4
TIEfigh1er-pil0t <marcin.garus@gmail.com>
2021-06-06 21:08:03 +0200

    passEnc commit

--- src/main/java/pl/garusm/charitydonation/config/WebSecurityConfig.java
+++ src/main/java/pl/garusm/charitydonation/config/WebSecurityConfig.java
@@ -7,16 +7,17 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    DataSource dataSource;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -25,7 +26,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .withUser("admin").password(passwordEncoder().encode("admin"))
+                .roles("ADMIN", "USER");
     }
 
     @Override
