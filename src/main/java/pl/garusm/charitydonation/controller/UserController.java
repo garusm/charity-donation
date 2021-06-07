e42c053151fa909c5b0b6323221222df203eea8b
TIEfigh1er-pil0t <marcin.garus@gmail.com>
2021-06-07 18:51:08 +0200

    user auth /80

--- src/main/java/pl/garusm/charitydonation/controller/UserController.java
+++ src/main/java/pl/garusm/charitydonation/controller/UserController.java
@@ -1,8 +1,11 @@
 package pl.garusm.charitydonation.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import pl.garusm.charitydonation.entity.User;
 
 @Controller
@@ -12,5 +15,11 @@ public class UserController {
     public String registerUser(Model model){
         model.addAttribute("user", new User());
         return "/register";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin2(@AuthenticationPrincipal UserDetails customUser) {
+        return "this is user " + customUser;
     }
 }
