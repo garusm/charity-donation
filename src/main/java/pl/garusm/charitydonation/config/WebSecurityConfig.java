6a63396596d8925f1299472beb7a26b8c7a7d2a1
TIEfigh1er-pil0t <marcin.garus@gmail.com>
2021-06-10 20:35:53 +0200

    security reformat start /WebSecurityConfig

--- src/main/java/pl/garusm/charitydonation/config/WebSecurityConfig.java
+++ src/main/java/pl/garusm/charitydonation/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package pl.garusm.charitydonation.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +21,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery("select email,password,enabled from users where email = ?")
+                .authoritiesByUsernameQuery("select email,authority from authorities where email = ?")
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
